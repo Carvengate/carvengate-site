8bdc46cb984ec6c3628cf864f497872cf01a609f
Martins Aloba <aloba.martins@gmail.com>
2021-03-25 12:12:16 +0100

    add base style for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Montserrat', 'Roboto'],
+      serif: ['Montserrat'],
+      mono: ['Montserrat'],
+      display: ['Montserrat'],
+      body: ['Montserrat'],
+      Jost: ['Jost', '"Open Sans"', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'blue-550': '#0066CC',
+        'blue-950': '#0E258D',
+        'blue-trans': 'rgba(0, 102, 204, 0.04)',
+      },
+      spacing: {
+        '17rem': '17rem',
+        '30rem': '30rem',
+        '37rem': '37rem',
+        '64rem': '64rem',
+        '70rem': '70rem'
+      }
+    },
   },
   variants: {
     extend: {},
